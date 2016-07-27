dede5a4050667eddf143cecc0174b541c3b4683e
rais <rais@SSD-16-0035.corp.softwaysolutions.com>
2016-07-27 15:15:44 +0530

    Header elements object Repository completed in About class

--- src/test/java/pageObjectFactory/About.java
+++ src/test/java/pageObjectFactory/About.java
@@ -11,7 +11,18 @@ import org.openqa.selenium.support.PageFactory;
 public class About {
 	WebDriver driver;
 	@FindBy(xpath = "//div[@id='menu']/ul/li[1]")
-	public WebElement about;
+	public WebElement About;
+	@FindBy(xpath ="//div[@id='menu']/ul/li[2]")
+	public WebElement Product;
+	@FindBy(xpath ="//div[@id='menu']/ul/li[3]")
+	public WebElement Resource;
+	@FindBy(xpath ="//div[@id='menu']/ul/li[4]")
+	public WebElement Support;
+	@FindBy(xpath ="//div[@id='menu']/ul/li[5]")
+	public WebElement Warranty;
+	@FindBy(xpath ="//div[@id='menu']/ul/li[6]")
+	public WebElement Registration;
+	
 	
 	@FindBys( {
 			@FindBy(xpath ="//div[@id='menu']/ul/li[1]"),
@@ -21,11 +32,53 @@ public class About {
 	public List<WebElement> AboutImgLinks;
 	
 	@FindBys( {
-		@FindBy(xpath ="//div[@id='menu']/ul/li[1]"),
-		@FindBy(className = "sub-menu-right"),
-		@FindBy(tagName = "li")
-		} )
+			@FindBy(xpath ="//div[@id='menu']/ul/li[1]"),
+			@FindBy(className = "sub-menu-right"),
+			@FindBy(tagName = "li")
+			} )
 	public List<WebElement> AboutLinks;
+	
+	@FindBys( {
+			@FindBy(xpath ="//div[@id='menu']/ul/li[2]"),
+			@FindBy(className = "sub-menu-left"),
+			@FindBy(tagName = "li")
+			} )
+	public List<WebElement> ProductImgLinks;
+
+	@FindBys( {
+			@FindBy(xpath ="//div[@id='menu']/ul/li[2]"),
+			@FindBy(className = "sub-menu-right"),
+			@FindBy(tagName = "li")
+		} )
+	public List<WebElement> ProductLinks;
+	
+	@FindBys( {
+			@FindBy(xpath ="//div[@id='menu']/ul/li[3]"),
+			@FindBy(className = "sub-menu-left"),
+			@FindBy(tagName = "li")
+			} )
+	public List<WebElement> ResourceImgLinks;
+
+	@FindBys( {
+			@FindBy(xpath ="//div[@id='menu']/ul/li[3]"),
+			@FindBy(className = "sub-menu-right"),
+			@FindBy(tagName = "li")
+		} )
+	public List<WebElement> ResourceLinks;
+	
+	@FindBys( {
+			@FindBy(xpath ="//div[@id='menu']/ul/li[4]"),
+			@FindBy(className = "sub-menu-left"),
+			@FindBy(tagName = "li")
+			} )
+	public List<WebElement> SupportImgLinks;
+
+	@FindBys( {
+			@FindBy(xpath ="//div[@id='menu']/ul/li[4]"),
+			@FindBy(className = "sub-menu-right"),
+			@FindBy(tagName = "li")
+		} )
+	public List<WebElement> SupportLinks;
 	
 	public About(WebDriver driver){
 		this.driver = driver;
@@ -37,10 +90,54 @@ public class About {
 	}
 	
 	public void clickAbout(){
-		about.click();
+		About.click();
 	}
 	
 	public void clickAboutLinks(int index){
 		AboutLinks.get(index).click();
+	}
+	
+	public void clickProducts(){
+		Product.click();
+	}
+	
+	public void clickProductImages(int index){
+		ProductImgLinks.get(index).click();
+	}
+			
+	public void clickProductLinks(int index){
+		AboutLinks.get(index).click();
+	}
+	
+	public void clickResource(){
+		Resource.click();
+	}
+	
+	public void clickResourceImages(int index){
+		ResourceImgLinks.get(index).click();
+	}
+			
+	public void clickResourceLinks(int index){
+		ResourceLinks.get(index).click();
+	}
+	
+	public void clickSupport(){
+		Support.click();
+	}
+	
+	public void clickSupportImages(int index){
+		SupportImgLinks.get(index).click();
+	}
+			
+	public void clickSupportLinks(int index){
+		SupportLinks.get(index).click();
+	}
+	
+	public void clickWarranty(){
+		Warranty.click();
+	}
+	
+	public void clickRegistration(){
+		Registration.click();
 	}
 }
