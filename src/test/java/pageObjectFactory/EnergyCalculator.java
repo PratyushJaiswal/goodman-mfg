baffd651ecfcd0f2285db457f8131e465f962a39
rais <rais@SSD-16-0035.corp.softwaysolutions.com>
2016-08-30 15:25:27 +0530

    Created page factory for Find a Dealer and Energy Calculator

--- src/test/java/pageObjectFactory/EnergyCalculator.java
+++ src/test/java/pageObjectFactory/EnergyCalculator.java
@@ -1,5 +1,54 @@
 package pageObjectFactory;
 
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
 public class EnergyCalculator {
+	WebDriver driver;
+	public EnergyCalculator(WebDriver driver){
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(id="address")
+	public WebElement address;
+	@FindBy(id="city")
+	public WebElement city;
+	@FindBy(id="state")
+	public WebElement state;
+	@FindBy(id="zip")
+	public WebElement zip;
+	@FindBy(id="state")
+	public WebElement findHome;
+	@FindBy(id="next1")
+	public WebElement next;
+	@FindBy(xpath="//div[@id='content']/div[1]/div[2]//div")
+	public List<WebElement> HomeAge;
+	@FindBy(xpath="//div[@id='content']/div[2]/div[2]//div")
+	public List<WebElement> HomeSize;
+	@FindBy(xpath="//div[@id='content']/div[4]/div[2]//div")
+	public List<WebElement> TotalWindows;
+	@FindBy(xpath="//div[@id='content']/div[3]/div[2]//div")
+	public List<WebElement> FinishedLevels;
+	
+	@FindBys( {
+		@FindBy(xpath="//div[@id='content']/div[5]/div[2]//div"),
+		@FindBy(xpath="//div[@id='content']/div[5]/div[3]//div")
+		} )
+	public List<WebElement> CurrentSystem;
+	
+	@FindBy(id="next0")
+	public WebElement back;
+	
+	@FindBy(xpath="//div[@id='content']/div[1]/div[2]//div")
+	public List<WebElement> HeatingRate;
+	
+	@FindBy(xpath="//div[@id='content']/div[2]/div[2]//div")
+	public List<WebElement> CoolingRate;
 
 }
--- src/test/java/pageObjectFactory/FindDealer.java
+++ src/test/java/pageObjectFactory/FindDealer.java
@@ -1,5 +1,50 @@
 package pageObjectFactory;
 
-public class FindDealer {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
+public class FindDealer {
+	WebDriver driver;
+	public FindDealer(WebDriver driver){
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath="//input[@id='dnn_ctr10007_GMDefault_ctl00_txtzipcode']")
+	public WebElement zip;
+	@FindBy(xpath="//select[@id='dnn_ctr10007_GMDefault_ctl00_ddlMiles']")
+	public WebElement miles;
+	@FindBy(xpath="//select[@id='dnn_ctr10007_GMDefault_ctl00_ddlfilter']")
+	public WebElement filterby;
+	@FindBy(xpath="//input[@id='dnn_ctr10007_GMDefault_ctl00_imgfinddealer']")
+	public WebElement search;
+	@FindBy(xpath="//span[@id='dnn_ctr10007_GMDefault_ctl00_lblcount']")
+	public WebElement resultCount;
+	
+	public void EnterZip(String zipcode){
+		zip.sendKeys(zipcode);
+	}
+	
+	public void selectMiles(String dropdown){
+		Select mile=new Select(miles);
+		mile.selectByValue(dropdown);
+	}
+	
+	public void selectFilter(String dropdown){
+		Select filter=new Select(filterby);
+		filter.selectByValue(dropdown);
+	}
+	
+	public void search(){
+		search.click();
+	}
+	
+	public void verifyResult(){
+		Assert.assertEquals(resultCount.getText(), "20 Results Found");
+	}
+	
 }
